3cc50d7a654a6c264fec0542975bd0da5012a2be
George <george.ridgway@protonmail.com>
2019-07-01 21:08:35 -0500

    Fixed Styling Issues
    refs issue #6

--- App/Modules/Video/index.js
+++ App/Modules/Video/index.js
@@ -1,14 +1,14 @@
 export const onVideoToggle = (obj) => {
-    const { paused } = obj.state
-    obj.setState({paused: !paused})
+  const { paused } = obj.state
+  obj.setState({paused: !paused})
 }
 
 export const onVideoEnd = (obj) => {
-    obj.setState({paused: true}, () => {
-        obj.player.seek(0)
-    })
+  obj.setState({paused: true}, () => {
+    obj.player.seek(0)
+  })
 }
 
 export const onVideoError = (e) => {
-    console.log(e);
+  console.log(e)
 }
