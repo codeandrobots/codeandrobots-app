c60a0493fa3d11558c93853a1c9263ea0b01cd3e
Chico <chico.charlesworth@googlemail.com>
2019-07-14 07:39:53 +0100

    Fix Robot List story and its snapshot
    Changes:
     - Remove scrollable prop from Robot List story as ScrollView fails
       to render in story snapshots, must be a storyshot bug :/
     - Change custom robot list item in Robot List story to be a video
       and remove atlas list item so that the scrollable prop does not
       need to be set to true
     - Update Stories test snapshot

--- App/Components/Stories/List.story.js
+++ App/Components/Stories/List.story.js
@@ -53,11 +53,10 @@ storiesOf('List', module)
     </List>
   ))
   .add('Robot List', () => (
-    <List title='Which Robot?' scrollable linedRows>
+    <List title='Which Robot?' linedRows>
       <CardListItem image={Images.robots.otto} title='Otto DIY' text='The open source robot that you can build yourself' />
       <CardListItem image={Images.robots.nybble} title='Nybble' text="The world's cutest open source robotic kitten" />
       <CardListItem image={Images.robots.simulator} title='Robot Simulator' text='No robot, no problem, simulator to the rescue' />
-      <CardListItem image={Images.robots.custom_robot} title='Custom Robot' text='Let’s see if your robot has what it takes' />
-      <CardListItem video={Videos.atlas} title='Otto DIY' text='The open source robot that you can build yourself' />
+      <CardListItem video={Videos.atlas} title='Custom Robot' text='Let’s see if your robot has what it takes' />
     </List>
   ))
