e5f74c3410a53c94be16c4d5d05eda0c60c85509
Chico <chico.charlesworth@googlemail.com>
2019-07-19 17:12:00 +0100

    Finish PlayerBottomNav component
    Refs #13
    
    Changes:
     - Improve PlayerBottomNav component to use slider and labels props
     - Update Player story that uses the PlayerBottomNav component

--- App/Components/Player/PlayerBottomNav/index.js
+++ App/Components/Player/PlayerBottomNav/index.js
@@ -2,42 +2,64 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import PropTypes from 'prop-types'
 
+import _ from 'lodash'
+import uuid from 'react-native-uuid'
+
 import { LabelRangeSliderInput, PlayerButton } from 'App/Components'
 
 import s from './Styles'
 
 export default class PlayerBottomNav extends Component {
   static propTypes = {
-    theme: PropTypes.string
+    theme: PropTypes.string,
+    slider: PropTypes.shape({
+      title: PropTypes.string,
+      labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+      defaultIndex: PropTypes.number,
+      onPress: PropTypes.func.isRequired
+    }),
+    skills: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        image: PropTypes.oneOfType([PropTypes.number, PropTypes.object]),
+        onPress: PropTypes.func.isRequired
+      }).isRequired
+    )
   }
 
   render () {
-    const { style = undefined, theme = 'default' } = this.props
+    const { style = undefined, theme = 'default', slider, skills = [] } = this.props
+
+    // Show three skills per row
+    const maxSkillsByRow = 3
+    const skillsInThrees = _.chunk(skills, maxSkillsByRow)
+
+    // Add empty skills to last chunk if last chunk has less than three skills
+    const chunks = skillsInThrees.length
+    const skillCountInLastChunk = (chunks > 0) ? skillsInThrees[chunks - 1].length : 0
+    if (skillCountInLastChunk > 0) {
+      for (let i = skillCountInLastChunk; i < maxSkillsByRow; i++) {
+        skillsInThrees[chunks - 1].push(null)
+      }
+    }
 
     return (
       <View style={[s.footer, style]}>
-        <LabelRangeSliderInput
-          theme={theme}
-          title='Speed'
-          labels={['Slow', 'Normal', 'Fast']}
-          defaultIndex={1}
-          onPress={(index) => {}} />
+        {slider &&
+          <LabelRangeSliderInput theme={theme} {...slider} />
+        }
         <View style={s.skills}>
-          <View style={s.buttons}>
-            <PlayerButton theme={theme} text='Jump' onPress={() => {}} />
-            <PlayerButton theme={theme} text='Shuffle' onPress={() => {}} />
-            <PlayerButton theme={theme} text='Bow' onPress={() => {}} />
-          </View>
-          <View style={s.buttons}>
-            <PlayerButton theme={theme} text='Spin' onPress={() => {}} />
-            <PlayerButton theme={theme} text='Dance' onPress={() => {}} />
-            <PlayerButton theme={theme} text='Moonwalk' onPress={() => {}} />
-          </View>
-          <View style={s.buttons}>
-            <PlayerButton theme={theme} text='Chill' onPress={() => {}} />
-            <PlayerButton theme={theme} disabled onPress={() => {}} />
-            <PlayerButton theme={theme} disabled onPress={() => {}} />
-          </View>
+          {skillsInThrees.map((skills) => {
+            return (
+              <View key={uuid.v4()} style={s.buttons}>
+                {skills.map((skill) => {
+                  return (skill)
+                    ? <PlayerButton key={uuid.v4()} theme={theme} text={skill.title} onPress={skill.onPress} />
+                    : <PlayerButton key={uuid.v4()} theme={theme} disabled onPress={() => {}} />
+                })}
+              </View>
+            )
+          })}
         </View>
       </View>
     )
--- App/Components/Stories/Player.story.js
+++ App/Components/Stories/Player.story.js
@@ -4,11 +4,28 @@ import { FullScreenDecorator } from './Decorators'
 
 import { PlayerBottomNav } from '../Player'
 
+const slider = {
+  title: 'Speed',
+  labels: ['Slow', 'Normal', 'Fast'],
+  defaultIndex: 1,
+  onPress: () => {}
+}
+
+const skills = [
+  {title: 'Jump', onPress: () => {}},
+  {title: 'Shuffle', onPress: () => {}},
+  {title: 'Bow', onPress: () => {}},
+  {title: 'Spin', onPress: () => {}},
+  {title: 'Dance', onPress: () => {}},
+  {title: 'Moonwalk', onPress: () => {}},
+  {title: 'Chill', onPress: () => {}}
+]
+
 storiesOf('Player', module)
   .addDecorator(FullScreenDecorator)
   .add('Bottom Nav', () => (
-    <PlayerBottomNav />
+    <PlayerBottomNav slider={slider} skills={skills} />
   ))
   .add('Bottom Nav Light', () => (
-    <PlayerBottomNav theme='light' />
+    <PlayerBottomNav theme='light' slider={slider} skills={skills} />
   ))
