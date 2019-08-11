f3d97a40be102327f255d7fc14ce0d96a340a1f9
OsosKP <ososkp@gmail.com>
2019-08-11 13:59:50 +0100

    Create BuildRobot Screen
    Add BuildRobot/index.js and BuildRobot/Screen.js
    
    Titles, text and image paths currently hardcoded in index.
    
    No functionality - no onPress navigation from screen yet.
    
    Currently using image CardListItems, will eventually use Video.
    
    Added module test for BuildRobot screen.

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -15,5 +15,13 @@ export default {
     nybble: require('App/Images/Robots/Nybble/nybble.png'),
     custom_robot: require('App/Images/Robots/Custom_Robot/custom_robot.png'),
     simulator: require('App/Images/Robots/Simulator/simulator.png')
+  },
+  buildRobots: {
+    otto: {
+      step1: require('App/Images/BuildRobot/Otto/step1.png'),
+      step2: require('App/Images/BuildRobot/Otto/step2.png'),
+      step3: require('App/Images/BuildRobot/Otto/step3.png'),
+      step4: require('App/Images/BuildRobot/Otto/step4.png')
+    }
   }
 }
