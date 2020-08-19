1a3f7ea32ce8431d275bdca014a7a0b0f30a5aa1
Chico <chico.charlesworth@googlemail.com>
2020-08-19 09:15:41 +0100

    Fix left & right commands used by code lab in Otto config, updated test snapshots

--- App/Services/Client/Otto/Config.js
+++ App/Services/Client/Otto/Config.js
@@ -65,8 +65,8 @@ export default {
     { title: 'Wait', cmd: 'M 0', showDuration: true },
     { title: 'Forwards', cmd: 'M 1', showDuration: true },
     { title: 'Back', cmd: 'M 2', showDuration: true },
-    { title: 'Left', cmd: 'M 3', showDuration: true },
-    { title: 'Right', cmd: 'M 4', showDuration: true }
+    { title: 'Left', cmd: 'M 4', showDuration: true },
+    { title: 'Right', cmd: 'M 3', showDuration: true }
   ],
   skills: [
     {
